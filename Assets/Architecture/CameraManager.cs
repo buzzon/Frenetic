f9ece53b9ef84783ff9a2921dc28a0123328ad69
_buzz0n <buzzondev@gmail.com>
2019-06-18 03:50:49 +0500

    Initial commit

---
+++ Assets/Architecture/CameraManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 _offset;
+
+    void Awake()
+    {
+        _offset.z += transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = _target.position + _offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+    }
+}
