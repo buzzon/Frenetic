0ae81995380c039c31f4b885fb2ec26ee9748eb1
_buzz0n <buzzondev@gmail.com>
2019-06-19 00:55:31 +0500

    UseStepSound

--- Assets/Architecture/PlayerManager.cs
+++ Assets/Architecture/PlayerManager.cs
@@ -6,6 +6,8 @@ public class PlayerManager : MonoBehaviour
 {
     private Rigidbody2D body;
     private Animator animator;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip[] audioClips;
 
     private float horizontal;
     private float vertical;
@@ -18,6 +20,7 @@ public class PlayerManager : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,11 +32,17 @@ public class PlayerManager : MonoBehaviour
 
     private void FixedUpdate()
     {
-        animator.SetBool("WalkUp", vertical > 0.1);
-        animator.SetBool("WalkDown", vertical < -0.1);
-        animator.SetBool("WalkRight", horizontal > 0.1);
-        animator.SetBool("WalkLeft", horizontal < -0.1);
+        Animate();
+        Move();
+    }
 
+    private bool IsMove()
+    {
+        return vertical != 0 || horizontal != 0;
+    }
+
+    private void Move()
+    {
         if (horizontal != 0 && vertical != 0)
         {
             horizontal *= moveLimiter;
@@ -41,5 +50,24 @@ public class PlayerManager : MonoBehaviour
         }
 
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+
+        if (!audioSource.isPlaying && IsMove())
+            UseStepSound();
+    }
+
+    public void UseStepSound()
+    {
+        audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+        audioSource.volume = Random.Range(0.8f, 1f);
+        audioSource.pitch = Random.Range(0.8f, 1.1f);
+        audioSource.Play();
+    }
+
+    private void Animate()
+    {
+        animator.SetBool("WalkUp", vertical > 0.1);
+        animator.SetBool("WalkDown", vertical < -0.1);
+        animator.SetBool("WalkRight", horizontal > 0.1);
+        animator.SetBool("WalkLeft", horizontal < -0.1);
     }
 }
